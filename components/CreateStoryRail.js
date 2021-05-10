dcfdafd41cbd3c82c43f3fd668893fde63989dca
Syaril Nazirul Sazali <syarilnazirul.sazali@Syarils-MBP.realtek>
2021-05-10 16:45:43 +0800

    Add ring on story

--- components/CreateStoryRail.js
+++ components/CreateStoryRail.js
@@ -3,12 +3,12 @@ function CreateStoryRail({ profilePicUrl }) {
         <div className="w-28 h-48 flex-col justify-center items-center bg-white rounded-lg">
             <img className="h-4/6 w-full object-fill rounded-t-lg" src={profilePicUrl} alt="syaril" />
             <div className="flex justify-center -mt-4 w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 bg-blue-500 rounded-full text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 bg-blue-500 rounded-full text-white ring-4 ring-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                 </svg>
             </div>
 
-            <h3 className="flex justify-center text-sm text-center px-10 font-medium">Create story</h3>
+            <h3 className="flex justify-center text-sm text-center px-10 font-medium">Create Story</h3>
         </div>
     )
 }
--- components/StoryRailOption.js
+++ components/StoryRailOption.js
@@ -1,9 +1,9 @@
 function StoryRailOption({ imgUrl, name, profilePicUrl }) {
     return (
-        <div className="relative w-28 h-48">
+        <div className="relative w-28 h-48 transform transition duration-500 hover:scale-105">
             <img className="rounded-lg object-cover w-28 h-48" src={imgUrl} alt={name} />
-            <img className="w-12 h-12 rounded-full absolute top-2 left-2" src={profilePicUrl} alt={name} />
-            <h3 className="absolute bottom-2 left-2 text-md text-white">{name}</h3>
+            <img className="w-9 h-9 rounded-full absolute top-2 left-2 ring-4 ring-blue-500" src={profilePicUrl} alt={name} />
+            <h3 className="absolute bottom-2 left-2 text-md font-semibold text-white">{name}</h3>
         </div>
     )
 }
